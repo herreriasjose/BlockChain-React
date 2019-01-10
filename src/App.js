f6a891e63a606723aeb14d1a51f5eb18abc3e5ab
herreriasjose <herreriasjose@gmail.com>
2019-01-10 11:01:08 +0100

    Fix vulnerability.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React, { Component } from 'react';
-import chain from './chain.svg';
-import './App.css';
-import Layout from '../src/containers/Layout'
+import React, { Component } from "react";
+import chain from "./chain.svg";
+
+import "./App.css";
+import "../src/bootstrap/css/bootstrap.min.css";
+
+import Layout from "../src/containers/Layout";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-          <h1>A visual approach to BlockChains using React</h1>
-          <img src={chain} className="App-logo" alt="chain" />
-        </header>
-        <div>
-         <Layout />
-         </div>
+        <div className="App-header">
+          <div className="row">
+            <div className="col-1 col-sm-1 col-md-1 col-lg-4 col-xl-4" />
+            <div className="col-10 col-sm-10 col-md-10 col-lg-4 col-xl-4">
+              <h1 className="text-primary">
+                A visual approach to BlockChains using React
+              </h1>
+              <img src={chain} className="App-logo" alt="chain" />
+            </div>
+            <div className="col-1 col-sm-1 col-md-1 col-lg-4 col-xl-4" />
+          </div>
+        </div>
+        <div className="row p-2">
+          <div className="col-1 col-sm-1 col-md-1 col-lg-4 col-xl-4" />
+          <div className="col-10 col-sm-10 col-md-10 col-lg-4 col-xl-4">
+            <Layout />
+          </div>
+          <div className="col-1 col-sm-1 col-md-1 col-lg-4 col-xl-4" />
+        </div>
       </div>
     );
   }
